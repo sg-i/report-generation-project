ff7d236bc881276a279051e4638f81cc3c111c5b
4017 Сгадов И.В <sgadov2vanya@yandex.ru>
2024-08-27 01:57:56 +0300

    init

--- report-generator-service/src/app.service.ts
+++ report-generator-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Генератор jahdj';
+    return 'Генератор чистый или нет';
   }
 }
--- report-requester-service/src/index.ts
+++ report-requester-service/src/index.ts
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Запросчик');
+  res.send(JSON.stringify(process.env));
 });
 
 app.listen(port, () => {
